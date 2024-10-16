5aa3f256a3fa38a7d024a7202b8c23ab71c64a84
zzstoatzz <thrast36@gmail.com>
2024-10-15 21:14:26 -0500

    slackbot

--- examples/memory_between_flows.py
+++ examples/memory_between_flows.py
@@ -6,13 +6,13 @@ thread_id = "test-thread"
 @cf.flow(thread=thread_id)
 def flow_1():
     task = cf.Task("get the user's name", result_type=str, interactive=True)
-    return task
+    return task.run()
 
 
 @cf.flow(thread=thread_id)
 def flow_2():
     task = cf.Task("write the user's name backwards, if you don't know it, say so")
-    return task
+    return task.run()
 
 
 if __name__ == "__main__":
